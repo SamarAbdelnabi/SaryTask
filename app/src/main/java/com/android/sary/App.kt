6a00070901e977d89d926dc9b848cff7de57ccfb
samarabdelnabi <samar_abdelnabi@hotmail.com>
2022-04-09 23:59:20 +0300

    fix lint error

--- app/src/main/java/com/android/sary/App.kt
+++ app/src/main/java/com/android/sary/App.kt
@@ -4,23 +4,16 @@ import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.ProcessLifecycleOwner
-import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import javax.inject.Inject
 
 @HiltAndroidApp
-class App: Application(), LifecycleObserver, Configuration.Provider  {
+class App: Application(), LifecycleObserver {
 
 
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
-
-
-    override fun getWorkManagerConfiguration() =
-        Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .build()
 
     override fun onCreate() {
         super.onCreate()
